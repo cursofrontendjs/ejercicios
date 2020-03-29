81e9f6ca4e2b70f53e3d9943e2b8fd508d9fbbd2
Quetzal Saez <usr@Quetzals-MacBook-Pro.local>
2020-03-29 17:51:59 -0300

    ejercicio6

---
+++ ejercicio-6/main.js
@@ -0,0 +1,9 @@
+var colores = ['Azul', 'Amarillo', 'Rojo', 'Gris'];
+
+var variable1 = colores[2] + ', ' + colores[3] + ', ' + colores[0] + ', ' + colores[1];
+var variable2 = colores[0] + '+' + colores[2] + '+' + colores[3] + '+' + colores[3] + '+' + colores[3] + '+' + colores[0];
+var variable3 = "'" + colores[1] + "', " + "'" + colores[0] +  "', " +  "'" + colores[2] + "'";
+
+console.log(variable1);
+console.log(variable2);
+console.log(variable3);
