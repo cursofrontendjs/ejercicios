3b12f76cbb70670350bbcb46a390b3973767d365
Quetzal Saez <usr@Quetzals-MacBook-Pro.local>
2020-03-29 18:45:33 -0300

    ejercicio8

---
+++ ejercicio-8/main.js
@@ -0,0 +1,11 @@
+var a = 10, 
+    b = 20, 
+    c = 30;
+
+if (a > b && a > c) {
+    console.log('a es el mayor');
+} else if (b > c && b > a) {
+    console.log('b es el mayor');
+} else {
+    console.log('c es el mayor');
+}
