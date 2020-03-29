42dc9913f7e6c0f69d05ed93b066fa9505df6560
Quetzal Saez <usr@Quetzals-MacBook-Pro.local>
2020-03-29 18:12:52 -0300

    ejercicio 7

---
+++ ejercicio-7/main.js
@@ -0,0 +1,15 @@
+var a = 5,
+    b = 8;
+
+if (b > a) {
+    console.log('el primer número es mayor al segundo');
+};
+if (b > 0) {
+    console.log('el numero es positivo');
+};
+if (a < 0 || a != 0) {
+    console.log('el número es negativo o distinto de cero');
+};
+if (a++ >= !b) {
+    console.log('Incrementar en uno el valor del primer número no lo hace mayor o igual que el segundo');
+};
