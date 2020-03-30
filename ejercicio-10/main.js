6fb532c3bb956b577240cee817ab34ae8537d277
Quetzal Saez <usr@Quetzals-MacBook-Pro.local>
2020-03-29 21:30:39 -0300

    ejercicio 10

---
+++ ejercicio-10/main.js
@@ -0,0 +1,9 @@
+function mayorMenor(a, b) {
+    if (a > b) {
+        console.log('el primer número es mayor');
+    } else {
+        console.log('el segundo número es mayor');
+    }
+}
+
+mayorMenor(3, 7);
