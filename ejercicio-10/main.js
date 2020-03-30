de7d9a72fabd43a2b11b09368e031634f3b4ca97
Quetzal Saez <usr@Quetzals-MacBook-Pro.local>
2020-03-29 21:37:25 -0300

    ejercicio 10 completo

--- ejercicio-10/main.js
+++ ejercicio-10/main.js
@@ -1,9 +1,19 @@
-function mayorMenor(a, b) {
+function numeroPar(a, b) {
     if (a > b) {
         console.log('el primer número es mayor');
     } else {
         console.log('el segundo número es mayor');
     }
+
+    if (a%2 == 0 && b%2 == 0) {
+        console.log('los dos numeros son pares');
+    } else if (a%2 == 0) {
+        console.log('el primer número es par');
+    } else if (b%2 == 0) {
+        console.log('el número '+ b + ' es par');
+    } else {
+        console.log('ninguno es par');
+    }
 }
 
-mayorMenor(3, 7);
+numeroPar(3, 7);
