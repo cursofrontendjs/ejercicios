be8f32fe7f9517da6d5cc65b889d380d2184239a
Quetzal Saez <usr@Quetzals-MacBook-Pro.local>
2020-04-03 00:06:55 -0300

    gsap4-1

--- gsap/ejercicio-4/main.js
+++ gsap/ejercicio-4/main.js
@@ -1,21 +1,20 @@
-gsap.from(".box", {
-  duration: 2,
-  scale: 0.5, 
-  rotation: 10,
-  opacity: 0, 
-  delay: 0.5, 
-  stagger: 0.2,
-  ease: "elastic", 
-});
-
-document.querySelectorAll(".box").forEach(function(box) {
-  box.addEventListener("click", function() {
-    gsap.to(".box", {
-      duration: 0.5, 
-      opacity: 0, 
-      y: -100, 
-      stagger: 0.1,
-      ease: "power1"
-    });
+document.getElementById('aparecer').addEventListener('click', function() {
+  gsap.to(".box", {
+    duration: 2,
+    scale: 1, 
+    rotation: 0,
+    opacity: 1, 
+    delay: 0.5, 
+    stagger: 0.2,
+    ease: "elastic", 
   });
 });
+
+document.getElementById("desaparecer").addEventListener('click', function() {
+  gsap.to(".box", {
+    duration: 0.5, 
+    opacity: 0, 
+    y: -100, 
+    stagger: 0.1,
+    ease: "power1"
+})});
